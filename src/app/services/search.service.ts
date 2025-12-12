8a4938bd35615c4a7cb244683e14b6b11253f603
Lukas Bühler <me@lukasbuehler.ch>
2025-12-12 11:39:39 +0100

    visual fixes

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -24,7 +24,7 @@ export class SearchService {
   });
 
   spotSearchParameters = {
-    query_by: "name_search,description_search",
+    query_by: "name_search,description_search,address.formatted",
     sort_by: "_text_match:desc",
     per_page: 5,
     page: 1,
